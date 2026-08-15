ef5a432aee09203cb4802874dff7b84852f0dc96
Maikel Zanchi <mzanchiti@outlook.com>
2026-08-15 12:55:30 -0300

    feat: integra consulta de CEP com ViaCEP

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { consultarCep } = require("./services/cepService");
 
 const app = express();
 
@@ -10,13 +11,24 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/api/cep/:cep", (req, res) => {
+app.get("/api/cep/:cep", async (req, res) => {
   const cep = req.params.cep;
 
-  res.json({
-    mensagem: "CEP recebido com sucesso.",
-    cep: cep
-  });
+  try {
+    const dados = await consultarCep(cep);
+
+    res.json({
+      cep: dados.cep,
+      logradouro: dados.logradouro,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      uf: dados.uf
+    });
+  } catch (erro) {
+    res.status(500).json({
+      erro: "Não foi possível consultar o CEP."
+    });
+  }
 });
 
 module.exports = app;
