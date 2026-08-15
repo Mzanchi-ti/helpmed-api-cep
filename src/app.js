d2b1cbe6d6e6f1952e58b8da973d24167155addb
Maikel Zanchi <mzanchiti@outlook.com>
2026-08-15 12:45:21 -0300

    feat: adiciona rota inicial de consulta de CEP

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
     mensagem: "API Help Med CEP funcionando."
+  });
+});
+
+app.get("/api/cep/:cep", (req, res) => {
+  const cep = req.params.cep;
+
+  res.json({
+    mensagem: "CEP recebido com sucesso.",
+    cep: cep
   });
 });
 
